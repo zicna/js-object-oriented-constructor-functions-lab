cefb70b54e3cb65af96d9b36434d20ff2bed2e6c
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2021-09-21 16:56:49 -0500

    Done.

---
+++ index.js
@@ -0,0 +1,17 @@
+const Scooter = function(year, color, model){
+    this.year = year
+    this.color = color
+    this.model = model
+}
+
+const Driver = function (name, age, experience){
+    this.name = name
+    this.age = age
+    this.experience = experience
+
+}
+
+const PickupLocation = function(address, city){
+    this.address = address
+    this.city = city
+}
