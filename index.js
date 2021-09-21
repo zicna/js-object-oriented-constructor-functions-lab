1169d1f0a8b2e0eec51beb68d9c2632944b34e25
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2021-09-21 16:59:10 -0500

    commit tree 769573ae59cfdd44184fad395b6c921539ec9c57

--- index.js
+++ index.js
@@ -1,17 +1,35 @@
-const Scooter = function(year, color, model){
-    this.year = year
-    this.color = color
-    this.model = model
+//*Constructor functon declaration
+// const Scooter = function(year, color, model){
+//     this.year = year
+//     this.color = color
+//     this.model = model
+// }
+
+// const Driver = function (name, age, experience){
+//     this.name = name
+//     this.age = age
+//     this.experience = experience
+
+// }
+
+// const PickupLocation = function(address, city){
+//     this.address = address
+//     this.city = city
+// }
+
+//*Constructor function expresion
+function Scooter(year, color, model) {
+  this.year = year;
+  this.color = color;
+  this.model = model;
+}
+function Driver(name, age, experience) {
+  this.name = name;
+  this.age = age;
+  this.experience = experience;
 }
 
-const Driver = function (name, age, experience){
-    this.name = name
-    this.age = age
-    this.experience = experience
-
-}
-
-const PickupLocation = function(address, city){
-    this.address = address
-    this.city = city
-}
+function PickupLocation(address, city){
+        this.address = address
+        this.city = city
+    }
